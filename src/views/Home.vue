a2ebd419b12887a450dac5ac1903a1a176445e6f
Eleni <57090719+eleniaslanidou89@users.noreply.github.com>
2021-09-16 11:34:54 +0200

    added img- fetching data from API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,40 @@
 <template>
-  <div>
-    home
-  </div>
+  <main v-if="!loading">
+    show data
+  </main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">fetching data</div>
+        <img :src="loadingImage" class="w-24 m-auto" alt="">
+  </main>
 </template>
 
 <script>
 export default {
   name: "Home",
   components: {},
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require("../assets/hourglass.gif"),
+    };
+  },
   methods: {
     async fetchCovidData() {
       const res = await fetch("https://api.covid19api.com/");
       const data = await res.json();
-      return data
+      return data;
     },
   },
   async created() {
-    const data = await this.fetchCovidData()
-    console.log(data)
+    const data = await this.fetchCovidData();
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
   },
 };
 </script>
