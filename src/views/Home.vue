8a01bbb122c9cac4ee4224dc9b138a3ef494c01b
Eleni <57090719+eleniaslanidou89@users.noreply.github.com>
2021-09-17 13:23:26 +0200

    added data from API
    totalDeaths-Cases

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <main v-if="!loading">
     <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
   </main>
   <main class="flex flex-col align-center justify-center text-center" v-else>
     <div class="text-gray-500 text-3xl mt-10 mb-6">fetching data</div>
@@ -10,10 +11,12 @@
 
 <script>
 import DataTitle from '@/components/DataTitle'
+import DataBoxes from '@/components/DataBoxes'
 export default {
   name: "Home",
   components: {
     DataTitle,
+    DataBoxes,
   },
   data() {
     return {
@@ -37,7 +40,7 @@ export default {
     this.dataDate = data.Date;
     this.stats = data.Global;
     this.countries = data.Countries;
-    this.loading = false;console.log(dataDate)
+    this.loading = false;
   },
   
 };
